6695169e22b3c786c9e4bbbf34cf5c24d8fb9f24
Ummesalama <umme2509zr@gmail.com>
2024-11-08 09:37:46 +0530

    Add files via upload

---
+++ IG.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+
+    const filterButtons = document.querySelectorAll('.filter-btn');
+    const imageContainer = document.querySelector('.image-container');
+
+    filterButtons.forEach(button => {
+
+        button.addEventListener('click', filterImages);
+
+    });
+
+    function filterImages(e){
+
+        const category = e.target.getAttribute('data-filter');
+        const images = document.querySelectorAll('.image')
+
+        images.forEach(image => {
+
+            image.style.display = 'none';
+
+            if(category =="all" || image.classList.contains(category)){
+                image.style.display = 'block';
+            }
+        });
+
+    }
+
+
+});
